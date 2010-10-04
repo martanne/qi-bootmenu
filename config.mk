a5bc9eb8ff612a6efcb782213e8437515e5e184e
Marc Andre Tanner <mat@brain-dump.org>
2010-10-04 22:03:04 +0200

    Change einas pkg-config name to match EFL 1.0
    Signed-off-by: Marc Andre Tanner <mat@brain-dump.org>

--- config.mk
+++ config.mk
@@ -10,12 +10,12 @@ DATADIR = ${PREFIX}/share/qi-bootmenu
 LDFLAGS_STATIC = -static 
 PKG_CONFIG_STATIC_FLAGS = --static
 
-INCS = $(shell pkg-config ${PKG_CONFIG_FLAGS} --cflags eina-0 evas ecore ecore-evas)
+INCS = $(shell pkg-config ${PKG_CONFIG_FLAGS} --cflags eina evas ecore ecore-evas)
 
 # add -leina explicitly because it seems like it has to come before -lm in order for
 # static linking to work and pkg-config adds it the other way round
  
-LIBS = -lc -leina $(shell pkg-config ${PKG_CONFIG_FLAGS} --libs eina-0 evas ecore ecore-evas)
+LIBS = -lc -leina $(shell pkg-config ${PKG_CONFIG_FLAGS} --libs eina evas ecore ecore-evas)
 
 # XXX: -std=c99 
 
